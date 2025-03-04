dfe9b8d2c9653a6e7887b8a46ed32b69460a0320
Ankush Shinde <125575601+ankush9730@users.noreply.github.com>
2025-03-04 18:50:24 +0530

    Update IDoctorManagmentService.java
    Update/ Add some methdosrequired Methods

--- BootDataJpaProj01-CrudRepository/src/main/java/com/nt/service/IDoctorManagmentService.java
+++ BootDataJpaProj01-CrudRepository/src/main/java/com/nt/service/IDoctorManagmentService.java
@@ -5,4 +5,16 @@ import com.nt.entity.Doctor;
 public interface IDoctorManagmentService {
 
 	public String registrDoctor(Doctor doctor);
+	public long showDoctorsCount();
+	public String registerDoctorsAsGroup(Iterable<Doctor>list);
+	public String checkDoctorAvailabilityById(int id);
+	public Iterable<Doctor> findAllDoctors();
+	public Iterable<Doctor> findAllByIds(Iterable<Integer>ids);
+	public Doctor showDoctorById(int id);
+	public String fetchDoctorById(int id);
+	public Optional<Doctor> getDoctorById(int id);
+	public String registerOrUpdateDoctor(Doctor doctor);
+	public String removeAllDoctors();
+	public String removeAllDoctorsById(Iterable<Integer> ids);
+	public String removeDoctorByID(int id);
 }
